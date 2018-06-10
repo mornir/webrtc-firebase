1ade0aaff26941e9fadc606228f0f34f3df3f600
mornir <jeromepott@hotmail.com>
2018-06-10 19:42:46 +0200

    Work in one way

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Tutorials used:
 
 1.  https://www.grafikart.fr/tutoriels/javascript/webrtc-864
 1.  https://websitebeaver.com/insanely-simple-webrtc-video-chat-using-firebase-with-codepen-demo
+1.  https://dev.to/rynobax_7/creating-a-multiplayer-game-with-webrtc
--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const showMyFaceButton = document.querySelector('#start')
 const showFriendsFaceButton = document.querySelector('#receiver')
 const showMyFaceVideo = document.querySelector('#emitter-video')
 const showFriendsFaceVideo = document.querySelector('#receiver-video')
+
 const offerTextarea = document.querySelector('#offer')
 
 const yourId = Math.floor(Math.random() * 1000000000)
+
+let p = null
 
 import { database } from './firebase-init.js'
 
@@ -14,23 +17,14 @@ function readMessage(data) {
 
   if (sender != yourId) {
     console.log(msg)
-
-    /*   let p = new SimplePeer({
-      initiator: false,
-    }) */
+    if (p === null) {
+      p = new SimplePeer({
+        initiator: false,
+      })
+      bindEvents(p)
+    }
+    p.signal(msg)
   }
-
-  /*   if (sender != yourId) {
-    if (msg.ice != undefined)
-      pc.addIceCandidate(new RTCIceCandidate(msg.ice));
-    else if (msg.sdp.type == "offer")
-      pc.setRemoteDescription(new RTCSessionDescription(msg.sdp))
-        .then(() => pc.createAnswer())
-        .then(answer => pc.setLocalDescription(answer))
-        .then(() => sendMessage(yourId, JSON.stringify({ 'sdp': pc.localDescription })));
-    else if (msg.sdp.type == "answer")
-      pc.setRemoteDescription(new RTCSessionDescription(msg.sdp));
-  } */
 }
 
 function bindEvents(p) {
@@ -48,23 +42,24 @@ function bindEvents(p) {
   })
 
   p.on('stream', stream => {
+    console.log('streaming')
+    console.log(stream)
     showFriendsFaceVideo.srcObject = stream
   })
 }
 
 database.on('child_added', readMessage)
 
-async function startPeer(initiator) {
+showMyFaceButton.addEventListener('click', async e => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       video: true,
       audio: false,
     })
 
-    let p = new SimplePeer({
-      initiator,
+    p = new SimplePeer({
+      initiator: true,
       stream,
-      trickle: true,
     })
 
     bindEvents(p)
@@ -73,12 +68,4 @@ async function startPeer(initiator) {
   } catch (e) {
     console.log(e.message)
   }
-}
-
-showMyFaceButton.addEventListener('click', e => {
-  startPeer(true)
-})
-
-showFriendsFaceButton.addEventListener('click', e => {
-  startPeer(false)
 })
